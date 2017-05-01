93c14a279efc7e9433299bd603f84fc4335f6845
l4ner0 <Diegourbina1284@gmail.com>
2017-05-01 18:00:51 -0500

    Add files via upload
    recuperación de archivos

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 	if(isset($_POST['ingresarPuntaje'])){
@@ -22,4 +23,30 @@
 		<input type="submit" name="verPuntaje" value="Ver Puntaje">
 	</form>
 </body>
+=======
+<?php
+
+	if(isset($_POST['ingresarPuntaje'])){
+
+		header('Location: LLenadoDatos.php');
+	
+	}else if(isset($_POST['verPuntaje'])){
+		header('Location: verDegradado.php');
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Bienvenida</title>
+</head>
+<body>
+	<h2>Bienvenido <?php echo "USUARIO" ?> al programa de análisis de puntaje v1</h2>
+	<form method="POST">
+		<input type="submit" name="ingresarPuntaje" value="Nuevo Puntaje">
+		<input type="submit" name="verPuntaje" value="Ver Puntaje">
+	</form>
+</body>
+>>>>>>> e812d49c89e6e2fa8074a6166f68d18f8f3f454e
 </html>
--- logout.php
+++ logout.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 	session_start();
@@ -6,4 +7,14 @@
 
 	header('location: LLenadoDatos.php');
 
+=======
+<?php
+
+	session_start();
+
+	session_destroy();
+
+	header('location: LLenadoDatos.php');
+
+>>>>>>> e812d49c89e6e2fa8074a6166f68d18f8f3f454e
 ?>
